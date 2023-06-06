c343a960c10362ea9487b054dc72ec95ffc2e756
FrostFireFalcon <21627553-FrostFireFalcon@users.noreply.replit.com>
2023-06-06 20:06:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.title("My Learning App")
+st.subheader("Introducing Streamlit in Automate Everything with Python")
+st.write("""
+This is my learning Web App.
+Enjoy! :)
+""")
